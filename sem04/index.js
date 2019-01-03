edfb0e12cd408994fcb941a0a5056453d0b13b19
Toni Corral Sastre <toni@localhost.localdomain>
2019-01-03 19:16:09 +0100

    adding new chat for sem04

--- sem04/index.js
+++ sem04/index.js
@@ -4,18 +4,20 @@ var io = require('socket.io')(http);
 var userList = [];
 var count = 0;
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+
 io.on('connection', function(socket) {
   console.log('a user connected');
-  userList[count]="Anonimo";
+  userList[count] = "Anonimo";
   console.log(userList);
   
   socket.emit('id', count);
   count++;
-  socket.broadcast.emit('chat message', 'Nuevo usuario en el chat'); //Broadcast
+  // Broadcast
+  socket.broadcast.emit('chat message', 'Nuevo usuario en el chat');
   console.log('Nuevo usuario en el chat');
   io.emit('lista usuarios', userList);
 
@@ -23,23 +25,24 @@ io.on('connection', function(socket) {
     console.log('user disconnected');
   });
   
-  socket.on('change user',function(msg,id){
-  	console.log('New user: ' + msg);
-  	userList[id] = msg;
-  	console.log(id);
-  	console.log(userList);
-  	console.log('lista usuarios: ' +userList);
-  	io.emit('lista usuarios', userList);
-  	
+  socket.on('change user',function(msg, id) {
+    console.log('New user: ' + msg);
+    userList[id] = msg;
+    console.log(id);
+    console.log(userList);
+    console.log('lista usuarios: ' + userList);
+    io.emit('lista usuarios', userList);
   });
+
   socket.on('chat message', function(msg,locutor) {
     console.log('message to ' + locutor + ': ' + msg);
     //console.log('message: ' + user);
-    //io.emit('chat message', locutor+": "+msg);
-    socket.broadcast.emit('chat message', locutor+": "+msg);
+    //io.emit('chat message', locutor + ": " + msg);
+    socket.broadcast.emit('chat message', locutor + ": " + msg);
   });
 });
 
-http.listen(3000, function(){
+
+http.listen(3000, function() {
   console.log('listening on *:3000');
 });

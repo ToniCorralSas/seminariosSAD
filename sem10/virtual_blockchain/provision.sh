c0d1afb2e62c1a009195249800434d7b8d1330b9
Toni Corral Sastre <toni@localhost.localdomain>
2019-01-05 18:43:41 +0100

    doing sem10

---
+++ sem10/virtual_blockchain/provision.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+
+
+
+apt-get -y update
+apt-get -y upgrade
+#apt-get -y install xinit
+#apt-get -y install xfce4-session
+#apt-get -y install xfce4-terminal
+#apt-get -y install xfce4-panel
+#apt-get -y install xfce4-whiskermenu-plugin
+#apt-get -y install thunar
+
+# edit /etc/xdg/user-dirs.default   # https://www.linuxquestions.org/questions/slackware-14/renaming-xfce-default-folders-4175438596/
+#xdg-user-dirs-update --force
+apt-get -y install language-pack-es
+apt-get -y install language-pack-es-base
+#apt-get -y install language-support-es
+apt-get -y install aspell-es
+apt-get -y install myspell-es
+apt-get -y install manpages-es
+locale-gen es_ES
+locale-gen es_ES.UTF-8
+#dpkg-reconfigure locales
+update-locale LANG=es_ES.UTF-8
+update-locale LANGUAGE="es_ES"
+#dpkg-reconfigure locales
+echo "export LANGUAGE=es_ES" >> ~/.bashrc
+#echo "export LANG=es_ES" >> ~/.bashrc
+echo "setxkbmap es" >> ~/.bashrc
+
+#apt-get -y install lightdm
+#apt-get -y install lightdm-gtk-greeter*
+#systemctl enable lightdm
+#apt-get -y install nginx
+#apt-get -y install virtualbox-guest-dkms
+#apt-get -y install virtualbox-guest-utils
+#apt-get -y install virtualbox-guest-x11
+#apt-get -y install firefox
+#apt-get -y install git curl
+apt-get -y install nodejs npm
+#apt-get -y install libzmq3-dev
+#npm install zeromq
+#apt-get -y install mongodb
+apt-get -y autoremove
+#cd /tmp
+#wget http://www.multichain.com/download/multichain-1.0.7.tar.gz
+#tar xvzf multichain-1.0.7.tar.gz
+#cd multichain-1.0.7
+#mv multichaind multichain-cli multichain-util /usr/local/bin
+#sleep 100
+#multichaind chain1@172.28.128.4:2873
+#sleep 100
+#multichaind chain1 -daemon
+
+#userdel -r ubuntu
+#reboot
---
+++ virtual_test/test_vagrant.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+
+
+# see: https://nmrony.info/change-disk-size-of-a-vagrant-box/
+#$ vagrant plugin install vagrant-disksize
+
+#vagrant init ubuntu/bionic64
+# see also: https://app.vagrantup.com/peru/boxes/ubuntu-18.04-server-amd64
+vagrant box update
+vagrant up   # user: vagrant; pass: vagrant
+#vagrant reload
+#vagrant ssh
+#vagrant provision
+#vagrant halt
+#vagrant destroy
